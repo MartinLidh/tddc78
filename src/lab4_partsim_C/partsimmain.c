c63c2e740476b142947a8157f33326566dead364
marli994 <marli994@student.liu.se>
2016-05-17 15:44:02 +0200

    push to triolith

--- src/lab4_partsim_C/partsimmain.c
+++ src/lab4_partsim_C/partsimmain.c
@@ -151,9 +151,6 @@ int main(int argc, char* argv[]){
     }
     
     //send and recive particles
-    if(upCount>0){
-      printf("upcounts %d and me %d\n", upCount, me);
-    }
     if(me != 0){
       MPI_Ibsend(&upCount, 1, MPI_INT ,me-1,1,com, &sendReqs);
       MPI_Ibsend(sendUp, upCount, MPI_PART,me-1,2,com, &sendReqs);
